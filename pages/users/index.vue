8b8604f8f39f755d3f5ca3535350a72eed1ed91a
adam5096 <adamchou5096@gmail.com>
2024-04-15 14:16:49 +0800

    Nuxt3-09-静態資源的訪存

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
         <h1>目錄user的預設元件(首頁) /page/users/</h1>
-        <MyComponent></MyComponent>
+        <!-- <MyComponent></MyComponent> -->
         <!-- 用法一：位在components目錄內的原件都屬全局元件，省去書寫匯入語法import，改書寫檔案名稱就可直接使用 -->
-        <xxx></xxx>
+        <!-- <xxx></xxx> -->
         <!-- 用法二：駝峰寫法(單字的首字母大寫) 拼接目錄名+檔案名，也可直書寫檔案名稱 -->
-        <UserDialog></UserDialog>
+        <!-- <UserDialog></UserDialog> -->
+        
+        <!--  -->
+        <img src="/img/vue.png" alt="" width="200px">
+        <img src="~/assets/img/tailwindcss.png" alt="" width="200px">
     </div>
 </template>
 
