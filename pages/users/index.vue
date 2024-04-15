781e7486bfae291c534022edf3c6b695b7413c73
adam5096 <adamchou5096@gmail.com>
2024-04-15 16:44:50 +0800

    Nuxt3-11-取得數據

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,32 +1,45 @@
 <template>
     <div>
         <h1>目錄user的預設元件(首頁) /page/users/</h1>
-        <!-- <MyComponent></MyComponent> -->
-        <!-- 用法一：位在components目錄內的原件都屬全局元件，省去書寫匯入語法import，改書寫檔案名稱就可直接使用 -->
-        <!-- <xxx></xxx> -->
-        <!-- 用法二：駝峰寫法(單字的首字母大寫) 拼接目錄名+檔案名，也可直書寫檔案名稱 -->
-        <!-- <UserDialog></UserDialog> -->
-        
-        <!--  -->
-        <!-- <img src="/img/vue.png" alt="" width="200px"> -->
-        <!-- <img src="~/assets/img/tailwindcss.png" alt="" width="200px"> -->
+        <h2>{{ data }}</h2>
     </div>
 </template>
 
-<script lang="ts" setup>
-import MyComponent from '~/pages/users/my-component.vue'
-useHead({
-    title: '/pages/uers/index.vue',
-})
-const config = useRuntimeConfig()
-console.log(config.aaaa);
-// console.log(config.public.bbbb);
-console.log(config.public.apiBase);
-if (config.isServer) {
-    console.log('只在Server Side Render');
+<script setup>
+const config = {
+    baseURL: 'https://jsonplaceholder.typicode.com/',
+    // 請求攔截器
+    onRequest({ request, options }) {
+        console.log('送出邀請了!!!!!');
+        console.log(options);
+    },
+    // 回應攔截器
+    onResponse({ request, response, options }) {
+        console.log('對方同意了!!!!!');
+    }
 }
+// useLazyFetch,點擊具有換頁+請求資料的綜合功能按鈕時
+// 不等待請求資料回到客戶端後，先把新頁的靜態結構展示到頁面上，隨後才放上請求回來的資料內容
+const { data } = await useLazyFetch('/posts', {
+    method: 'GET',
+    params: { id: 3 },
+    ...config
+})
+// useFetch 點擊具有換頁+請求資料的綜合功能按鈕時
+// 會等待請求資料回到客戶端後，再一併連同新頁的靜態結構同時展示到頁面上
+// useFetch('/posts', {
+//     method: 'GET',
+//     baseURL: 'https://jsonplaceholder.typicode.com/'
+// }).then(res => {
+//     console.log(res);
+// })
+// useAsyncData('取得文章', () => $fetch('/posts', {
+//     method: 'GET',
+//     baseURL: 'https://jsonplaceholder.typicode.com/'
+// }).then(res => {
+//     console.log(res);
+// })
+// );
 </script>
 
-<style>
-
-</style>
+<style></style>
