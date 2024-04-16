7aec6ec43a7f9698a642416174bf7a8bf246e214
adam5096 <adamchou5096@gmail.com>
2024-04-16 15:57:02 +0800

    Nuxt3-19-预渲染 SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,33 @@
+let routesArr = []
+for (let i:any; i <= 10; i++){
+  routesArr.push(`/article/${i}`)
+}
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
-    //伺服器端可以渲染到的一些屬性與值(包含public目錄裡面的屬性與值)
     aaaa: 100,
     isServer: true,
-    public: {//客戶端只渲染得到public目錄裡的內容
+    public: {
       bbbb: 200,
       apiBase: 'https://www.google.com'
     }
   },
+  // 對指定路由個別設置
   routeRules: {
+    //關閉ssr,改csr
     '/xxx/yyy/zzz': { prerender: true },
+    //重定向
     '/admin/**': { ssr: false },
     '/old-page': { redirect: '/users/create-or-edit' },
+    //自訂頭部載物
     '/users/**': { static: true, headers: { 'cache-control': 'text-text' } },
+    //開啟同源政策
     '/api/aaaa': { cors: true }
+  },
+  nitro: {
+    prerender: {
+      routes: routesArr
+    }
   }
 })
