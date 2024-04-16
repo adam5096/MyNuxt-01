53a327ca7a3cb944df1a5d0570f33a9faef5bd9d
adam5096 <adamchou5096@gmail.com>
2024-04-16 14:08:46 +0800

    Nuxt3-17-部署 NodeServer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
+  //伺服器端可以渲染到的一些屬性與值(包含public目錄裡面的屬性與值)
     aaaa: 100,
     isServer: true,
-    public: {
+    public: {//客戶端只渲染得到public目錄裡的內容
       bbbb: 200,
       apiBase: 'https://www.google.com'
     }
