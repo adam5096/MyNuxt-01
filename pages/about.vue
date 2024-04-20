4a4548d70ed57b646a48200cf54306fe3bbce701
adam5096 <157099004+adam5096@users.noreply.github.com>
2024-04-20 22:25:38 +0800

    Delete pages directory

--- pages/about.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>I am About</h1>
-        <h2>2024前端發展</h2>
-    </div>
-</template>
--- pages/users/create-or-edit.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div>
-        <h1>create-or-edit</h1>
-        <h2>{{ counter }}</h2>
-    </div>
-</template>
-<script lang="ts" setup>
-const counter = useCounter()
-</script>
